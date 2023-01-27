b2a5617c336a3fce7621e2aa5879d9358e07d68a
ShurikMint <myatnyys@list.ru>
2023-01-27 18:49:03 +0300

    Start

---
+++ GamingSite/Controllers/AuthorizationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamingSite.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        [Route("Authorization")]
+        [HttpGet]
+        public ViewResult Authorization()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IAsyncResult UserVerification()
+        {
+
+        }
+    }
+}
